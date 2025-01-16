a944bc386172553b87dffcf179c1d5c06580fa79
Yasmine LOUAKED <yasmine.louaked@beyn.dz>
2025-01-16 17:03:39 +0100

    psw

--- src/app/components/password/password.component.ts
+++ src/app/components/password/password.component.ts
@@ -61,3 +61,60 @@ export class PasswordComponent  implements OnInit {
       });
   }
 }
+
+
+
+
+// import { ChangeDetectionStrategy, Component, signal } from '@angular/core';
+// import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
+// import {
+//   FormControl,
+//   FormsModule,
+//   ReactiveFormsModule,
+//   Validators,
+// } from '@angular/forms';
+// import { MatFormFieldModule } from '@angular/material/form-field';
+// import { MatInputModule } from '@angular/material/input';
+// import { merge } from 'rxjs';
+
+// /** @title Form field with error messages */
+// @Component({
+//   selector: 'form-field-error-example',
+//   templateUrl: 'form-field-error-example.html',
+//   styleUrl: 'form-field-error-example.css',
+//   imports: [
+//     MatFormFieldModule,
+//     MatInputModule,
+//     FormsModule,
+//     ReactiveFormsModule,
+//   ],
+//   changeDetection: ChangeDetectionStrategy.OnPush,
+// })
+// export class FormFieldErrorExample {
+//   readonly password = new FormControl('', [Validators.required]);
+//   readonly newpassword = new FormControl('', [Validators.required]);
+//   readonly confirmpassword = new FormControl('', [Validators.required]);
+//   errorMessage = signal('');
+
+//   constructor() {
+//     merge(this.password.statusChanges, this.password.valueChanges)
+//       .pipe(takeUntilDestroyed())
+//       .subscribe(() => this.updateErrorMessage());
+//   }
+
+//   updateErrorMessage() {
+//     if (this.password.hasError('required') && (this.newpassword.hasError('required') ) ) 
+//     {
+//       this.errorMessage.set('Ce champ est obligatoire');
+//     } else 
+//       {
+//       this.errorMessage.set('');
+//     }
+//   }
+
+//   protected readonly value = signal('');
+
+//   protected onInput(event: Event) {
+//     this.value.set((event.target as HTMLInputElement).value);
+//   }
+// }
